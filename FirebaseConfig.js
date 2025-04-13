84a0de58afbd3d841a4659c43fdbe35076ad7a34
docere-team <docere.team@gmail.com>
2025-04-14 00:33:47 +0530

    Create FirebaseConfig.js

---
+++ FirebaseConfig.js
@@ -0,0 +1,13 @@
+// firebaseConfig.js
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDGGuoM1RCdV-n9pkmw_c6wvGoejnu6rOE",
+    authDomain: "nation-bike.firebaseapp.com",
+    projectId: "nation-bike",
+    storageBucket: "nation-bike.appspot.com",
+    messagingSenderId: "25249080743",
+    appId: "1:25249080743:web:238adc1be73839fa740282"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
